acd6509fb3f1973f639bc6f6b6788e02f9d6d6fb
drkwdck <reshetnikov.e.a@mail.ru>
2020-11-04 22:46:57 +0300

    Правка теста

--- tb/MainTest.v
+++ tb/MainTest.v
@@ -1,33 +1,34 @@
 `timescale 1ns / 10ps
- module processor_tb();
- 
- 
-reg 				clk;
-reg				reset;
-reg 	[31:0]		SW ;
-wire	[31:0]	HEX;
 
-Main DUT(
-	.clk   ( clk   ),
-	.reset ( reset ),
-	.SW	 ( SW    ),
-	.HEX ( HEX   )
+module MainTest();
+
+reg clk;
+reg reset;
+reg [31:0] SW ;
+wire [31:0] HEX;
+
+Main MainUnitTest(
+  .clk (clk),
+  .reset (reset),
+  .SW (SW),
+  .HEX (HEX)
 );
 
 initial SW = 32'd30;
 
 initial begin
-reset = 1;
-#20;
-reset = 0;
-#20;
+  reset = 1;
+  #20;
+  reset = 0;
+  #20;
 end
 
 initial begin
   clk = 1'b0;
 
   forever begin
-  #15 clk = ~clk;
+    #15 
+    clk = ~clk;
     $display("%b", HEX);
   end 
 end
