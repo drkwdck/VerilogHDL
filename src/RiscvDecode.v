087109e350f1c70b2c17d64070927a67ab7963cc
drkwdck <reshetnikov.e.a@mail.ru>
2020-11-05 11:31:45 +0300

    1630

--- src/RiscvDecode.v
+++ src/RiscvDecode.v
@@ -1,12 +1,12 @@
 module RiscvDecode
 (
   input   [31:0]               fetched_instr_i,
-  output  reg [1:0]                ex_op_a_sel_o,
-  output  [2:0]                ex_op_b_sel_o,
-  output  [`ALU_OP_WIDTH-1:0]  alu_op_o,
+  output  reg [1:0]             ex_op_a_sel_o,
+  output  reg [2:0]                ex_op_b_sel_o,
+  output  reg [`ALU_OP_WIDTH-1:0]  alu_op_o,
   output                       mem_req_o,
   output                       mem_we_o,
-  output  [2:0]                mem_size_o,
+  output  reg [2:0]            mem_size_o,
   output                       gpr_we_a_o,
   output                       wb_src_sel_o,
   output                       illegal_instr_o,
@@ -15,20 +15,9 @@ module RiscvDecode
   output                       jarl_o
 );
 
-reg ex_op_a_sel[1:0], ex_op_b_sel[2:0], alu_op[`ALU_OP_WIDTH-1:0], 
-write_enable, mem_request, mem_size[2:0], illegal_instr, jal, jarl,
-gpr_we_a, wb_src_sel, branch;
+reg  write_enable, mem_request, illegal_instr, jal, jarl, gpr_we_a, wb_src_sel, branch;
 
 
-assign ex_op_b_sel_o[0] = ex_op_b_sel[0];
-assign ex_op_b_sel_o[1] = ex_op_b_sel[1];
-
-assign mem_size_o[0] = mem_size[0];
-assign mem_size_o[1] = mem_size[1];
-assign mem_size_o[2] = mem_size[2];
-
-assign alu_op_o = {alu_op[0], alu_op[1], alu_op[2], alu_op[3], alu_op[4], alu_op[5]};
-assign mem_size_o = {mem_size[0], mem_size[1], mem_size[2]};
 assign mem_we_o = write_enable;
 assign mem_req_o = mem_request;
 assign illegal_instr_o = illegal_instr;
@@ -49,16 +38,6 @@ always @(*) begin
   wb_src_sel <= 1'b0;
   branch <= 1'b0;
 
-  ex_op_a_sel[0] <= 1'b0;
-  ex_op_a_sel[1] <= 1'b0;
-  ex_op_b_sel[0] <= 1'b0;
-  ex_op_b_sel[0] <= 1'b0;
-
-  mem_size[0] <= 1'b0;
-  mem_size[1] <= 1'b0;
-  mem_size[2] <= 1'b0;
-
-
   case(fetched_instr_i[1:0])
       2'b11: begin
         if (fetched_instr_i[6:2] != 5'b11100 && fetched_instr_i[6:2] != 5'b00011) begin
@@ -70,37 +49,49 @@ always @(*) begin
             mem_request <= 1'b1;
             gpr_we_a <= 1'b1;
             wb_src_sel <= 1'b1;
-          end
-
-          `STORE_OPCODE: begin
-            write_enable <= 1'b1;
-            mem_request <= 1'b1;
+            ex_op_a_sel_o = `OP_A_RS1;
+            ex_op_b_sel_o = `OP_B_IMM_I;
+            mem_size_o =  `LDST_B;
+            alu_op_o = `ALU_ADD;
           end
 
           `OP_IMM_OPCODE: begin
             gpr_we_a <= 1'b1;
+            ex_op_a_sel_o = `OP_A_RS1;
+            ex_op_b_sel_o = `OP_B_IMM_U;
           end
 
           `AUIPC_OPCODE: begin
             gpr_we_a <= 1'b1;
-            ex_op_a_sel[0] <= 1'b1;
-            ex_op_a_sel[1] <= 1'b0;
-            ex_op_b_sel[0] <= 1'b0;
-            ex_op_b_sel[1] <= 1'b1;
+            ex_op_b_sel_o = `OP_B_IMM_U << 12;
+            alu_op_o = `ALU_ADD;
+            ex_op_a_sel_o = `OP_A_CURR_PC;
+          end
+
+          // ?
+          `STORE_OPCODE: begin
+            write_enable <= 1'b1;
+            mem_request <= 1'b1;
+            alu_op_o = `ALU_ADD;
+            ex_op_a_sel_o = `OP_A_RS1;
+            ex_op_b_sel_o = `OP_B_IMM_I;
           end
 
           `OP_OPCODE: begin
             gpr_we_a <= 1'b1;
-            ex_op_b_sel[1] <= 1'b1;
+            ex_op_a_sel_o = `OP_A_RS1;
+            ex_op_b_sel_o = `OP_B_RS2;
+            // alu_op_o = fetched_instr_i[31:25];
           end
 
           `LUI_OPCODE: begin
             gpr_we_a <= 1'b1;
-            ex_op_b_sel[1] <= 1'b1;
+            ex_op_b_sel_o = `OP_B_IMM_U;
+            ex_op_a_sel_o = `OP_A_ZERO;
+            alu_op_o = `ALU_ADD;
           end
 
           `BRANCH_OPCODE: begin
-            ex_op_b_sel[1] <= 1'b1;
             branch <= 1'b1;
           end
 
