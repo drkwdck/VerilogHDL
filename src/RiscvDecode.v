c8698ec14094ebb14ed84741df4404c1f8d59ee9
drkwdck <reshetnikov.e.a@mail.ru>
2020-11-08 15:01:11 +0300

    дошел до кодировки бренчей

--- src/RiscvDecode.v
+++ src/RiscvDecode.v
@@ -39,7 +39,7 @@ always @(*) begin
           `OP_OPCODE: begin 
             ex_op_a_sel_o <= `OP_A_RS1;
             ex_op_b_sel_o <= `OP_B_RS2;
-            gpr_we_a_o <= 1'b0;
+            gpr_we_a_o <= 1'b1;
             
             case (funct3)
               3'h0: begin
@@ -47,13 +47,11 @@ always @(*) begin
                   7'h0: begin
                     alu_op_o <= `ALU_ADD;
                     illegal_instr_o <= 0;
-                    gpr_we_a_o <= 1'b1;
                   end
 
                   7'h20: begin
                     alu_op_o <= `ALU_SUB;
                     illegal_instr_o <= 0;
-                    gpr_we_a_o <= 1'b1;
                   end
 
                   default:
@@ -66,7 +64,6 @@ always @(*) begin
                   7'h0: begin
                     alu_op_o <= `ALU_XOR;
                     illegal_instr_o <= 0;
-                    gpr_we_a_o <= 1'b1;
                   end
 
                   default:
@@ -161,11 +158,93 @@ always @(*) begin
 
           default:
             illegal_instr_o <= 1'b1;
+        
+        `OP_IMM_OPCODE: begin
+            ex_op_a_sel_o = `OP_A_RS1;
+            ex_op_b_sel_o = `OP_B_IMM_I;
+            case (funct3)
+                3'h0:
+                    alu_op_o <= `ALU_ADD;
+                3'h3:
+                    alu_op_o <= `ALU_SLTU;
+                3'h4:
+                    alu_op_o <= `ALU_XOR;
+                3'h6:
+                    alu_op_o <= `ALU_OR;
+                3'h7:
+                    alu_op_o <= `ALU_AND;
+                3'h1: begin
+                    case(funct7)
+                        7'h0:
+                            alu_op_o <= `ALU_SLL;
+                        default:
+                            illegal_instr_o <= 1;
+                    endcase
+                end
+                3'h2:
+                    alu_op_o <= `ALU_SLTS;
+                3'h3: begin
+                    case(funct7)
+                        7'h0:
+                            alu_op_o <= `ALU_SRL;
+                        7'h20:
+                            alu_op_o <= `ALU_SRA;
+                        default:
+                            illegal_instr_o <= 1;
+                    endcase
+                    end
+                default:
+                    illegal_instr_o <= 1;
+            endcase
+        end
+
+        `LOAD_OPCODE: begin
+            alu_op_o <= `ALU_ADD;
+            ex_op_a_sel_o <= `OP_A_RS1;
+            ex_op_b_sel_o <= `OP_B_IMM_I;
+            case(funct3)
+                3'h0:
+                    mem_size_o <= `LDST_B;
+                3'h1:
+                    mem_size_o <= `LDST_H;
+                3'h2:
+                    mem_size_o <= `LDST_W;
+                3'h4:
+                    mem_size_o <= `LDST_BU;
+                3'h5:
+                    mem_size_o <= `LDST_HU;
+                default:
+                    illegal_instr_o <= 1;
+            endcase
+        end
+
+        `STORE_OPCODE: begin
+            alu_op_o <= `ALU_ADD;
+            ex_op_a_sel_o <= `OP_A_RS1;
+            ex_op_b_sel_o <= `OP_B_IMM_S;
+            case(funct3)
+                3'h0:
+                    mem_size_o <= `LDST_B;
+                3'h1:
+                    mem_size_o <= `LDST_H;
+                3'h2:
+                    mem_size_o <= `LDST_W;
+                default:
+                    illegal_instr_o <= 1;
+            endcase
+        end
+
+        `BRANCH_OPCODE: begin
+            ex_op_a_sel_o <= `OP_A_RS1;
+            ex_op_b_sel_o <= `OP_B_RS2;
+            
+        end
        endcase
       end
       
       default:
         illegal_instr_o <= 1'b1;
+
   endcase
 end
 endmodule
